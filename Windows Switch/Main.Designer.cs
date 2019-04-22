9b9375251b1837530adf9b13a5d82e4ad69a0e28
Dinh Tran <robowonder@outlook.com>
2019-04-22 16:10:22 +0700

    Init project

---
+++ Windows Switch/Main.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Windows_Switch
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mniOpenFrom = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows Virtual Desktop Switch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(122, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1. Create new Virtual Desktop";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(122, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(399, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "2. Press the key combination Ctrl + Windows";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(122, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "3. Scroll wheel button";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(122, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(266, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "2. Virtual Desktop will change";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniOpenFrom,
+            this.toolStripSeparator1,
+            this.mniExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(238, 58);
+            // 
+            // mniOpenFrom
+            // 
+            this.mniOpenFrom.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mniOpenFrom.Name = "mniOpenFrom";
+            this.mniOpenFrom.Size = new System.Drawing.Size(237, 24);
+            this.mniOpenFrom.Text = "Virtual Desktop Switch";
+            this.mniOpenFrom.Click += new System.EventHandler(this.mniOpenFrom_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(237, 24);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(234, 6);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(512, 286);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(115, 48);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 371);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "Virtual Desktop Switch";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mniOpenFrom;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Windows Switch/Main.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Microsoft.Win32;
+
+namespace Windows_Switch
+{
+    public partial class Main : Form
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+        public string AppName = "VirtualDesktopSwitch";
+        public static List<Keys> keysDown = new List<Keys>();
+        private bool openFrom = false;
+        private bool exit = false;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyDown += OnKeyDown;
+            m_GlobalHook.KeyUp += OnKeyUp;
+
+            m_GlobalHook.MouseWheelExt += HookManager_MouseWheelExt;
+        }
+
+        private void HookManager_MouseWheelExt(object sender, MouseEventExtArgs e)
+        {
+            if (WIN() && CTRL())
+            {
+                if (e.Delta > 0)
+                {
+                    SendKeys.Send("{RIGHT}");
+                }
+                else
+                {
+                    SendKeys.Send("{LEFT}");
+                }
+                e.Handled = true;
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            while (keysDown.Contains(e.KeyCode))
+            {
+                keysDown.Remove(e.KeyCode);
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (keysDown.Contains(e.KeyCode) == false)
+            {
+                keysDown.Add(e.KeyCode);
+            }
+        }
+
+        public static bool CTRL()
+        {
+            if (keysDown.Contains(Keys.LControlKey) ||
+                keysDown.Contains(Keys.RControlKey) ||
+                keysDown.Contains(Keys.Control) ||
+                keysDown.Contains(Keys.ControlKey))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool WIN()
+        {
+            if (keysDown.Contains(Keys.LWin) ||
+                keysDown.Contains(Keys.RWin))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void AutoStart()
+        {
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (rkApp.GetValue(AppName) == null || rkApp.GetValue(AppName).ToString() != Application.ExecutablePath)
+            {
+                openFrom = true;
+                rkApp.SetValue(AppName, Application.ExecutablePath);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisplayOnTray();
+            if (!exit)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void mniOpenFrom_Click(object sender, System.EventArgs e)
+        {
+            openFrom = true;
+            Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void mniExit_Click(object sender, System.EventArgs e)
+        {
+            exit = true;
+            Close();
+        }
+
+        private void Main_Load(object sender, System.EventArgs e)
+        {
+            AutoStart();
+            if (!openFrom)
+            {
+                DisplayOnTray();
+            }
+        }
+        private void DisplayOnTray()
+        {
+            Hide();
+            notifyIcon1.Visible = true;
+            ShowInTaskbar = false;
+        }
+
+        private void btnOk_Click(object sender, System.EventArgs e)
+        {
+            DisplayOnTray();
+        }
+    }
+}
